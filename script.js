6fc28a5c94a0a04ea3cd9e67d35ee6a2e0df4530
www-web-site <329blogertop228@gmail.com>
2025-05-29 16:53:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,303 @@
+const data = [
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'nexia',
+        carPrice: 10000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tahoe',
+        carPrice: 70000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'lacceti',
+        carPrice: 15000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'malibu',
+        carPrice: 17000,
+        descr: 'lorem ipsum sodor'
+    },
+    {
+        carName: 'tracker',
+        carPrice: 13000,
+        descr: 'lorem ipsum sodor'
+    }
+]
+
+const wrapper = document.createElement('div');
+wrapper.classList.add('flex', 'flex-wrap', 'bg-gray-400',);
+document.body.append(wrapper);
+
+data.forEach(element =>{
+    const card = document.createElement('div')
+    card.classList.add('flex', 'flex-col', 'w-[210px]', 'h-[210px]', 
+        'bg-black', 'p-2', 'm-4', 'justify-center', 'items-center', 'rounded-md')
+    wrapper.append(card)
+
+    const  name = document.createElement('h1')
+    name.classList.add('text-gray-800', 'text-3xl', 'font-sans')
+    name.textContent = element.carName
+    card.append(name)
+
+    const price = document.createElement('h3')
+    price.classList.add('text-gray-500','text-2xl', 'font-sans')
+    price.textContent = element.carPrice
+    card.append(price)
+
+    const text = document.createElement('p')
+    text.classList.add('text-gray-700', 'font-sans')
+    text.textContent = element.descr
+    card.append(text)
+})
